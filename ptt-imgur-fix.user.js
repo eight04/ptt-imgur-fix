a2da17247d7c5de2a2c7cbf089dc81f9c12016a1
eight <eight04@gmail.com>
2017-03-19 07:41:40 +0800

    Release v0.1.1

--- ptt-imgur-fix.user.js
+++ ptt-imgur-fix.user.js
@@ -3,7 +3,7 @@
 // @description	修正 Imgur 在 PTT 上的問題
 // @namespace   eight04.blogspot.com
 // @include     https://www.ptt.cc/bbs/*.html
-// @version     0.1.0
+// @version     0.1.1
 // @author		eight
 // @homepage	https://github.com/eight04/ptt-imgur-fix
 // @supportURL	https://github.com/eight04/ptt-imgur-fix/issues
