ea56bb19960bb9e637f9ca64bad4760f54dec0bf
eight04 <eight04@gmail.com>
2026-09-14 00:35:21 +0800

    0.10.6

--- ptt-imgur-fix.user.js
+++ ptt-imgur-fix.user.js
@@ -7,7 +7,7 @@
 // @match https://www.ptt.cc/man/*.html
 // @match https://www.ptt.cc/man/*.html?*
 // @match https://term.ptt.cc/
-// @version     0.10.5
+// @version     0.10.6
 // @author		eight
 // @homepage	https://github.com/eight04/ptt-imgur-fix
 // @supportURL	https://github.com/eight04/ptt-imgur-fix/issues
